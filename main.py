64f66fd7e6aa3fde04ffdf049c1cb29a0f3378e5
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-19 16:15:52 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#== Decoding ==#
+
+import base64
+
+base64_message = 'UHl0aG9uIGlzIGZ1bg=='
+base64_bytes = base64_message.encode('ascii')
+message_bytes = base64.b64decode(base64_bytes)
+message = message_bytes.decode('ascii')
+
+print(message)
